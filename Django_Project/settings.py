4e9ab1c4dd44a04202d9a752558b14407172c894
Ranveer Maskara <ranveer.maskara@gmail.com>
2025-06-30 23:31:47 +0530

    Fix ALLOWED_HOSTS for deployment

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-render-app-name.onrender.com']
+ALLOWED_HOSTS = ['django-blog-pyg2.onrender.com']
+
 
 
 
