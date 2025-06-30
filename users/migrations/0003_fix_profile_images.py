d63aa3d13ef1b3bba19121ee565ae27c589f47c3
Ranveer Maskara <ranveer.maskara@gmail.com>
2025-07-01 01:09:35 +0530

    Fix broken profile image paths using migration

---
+++ users/migrations/0003_fix_profile_images.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+def fix_images(apps, schema_editor):
+    Profile = apps.get_model('users', 'Profile')
+    default_url = 'https://res.cloudinary.com/dbdqfgqti/image/upload/v1751310469/default_oygkle.jpg'
+
+    for profile in Profile.objects.all():
+        if str(profile.image).startswith('media/') or str(profile.image).startswith('/media/'):
+            profile.image = default_url
+            profile.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', 'xxxx_previous_migration_name'),  # Replace with actual last migration
+    ]
+
+    operations = [
+        migrations.RunPython(fix_images),
+    ]
